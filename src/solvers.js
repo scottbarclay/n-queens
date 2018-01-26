5b1e863127e19bc51b82437020ddbe3408896fb5
scottbarclay <scottdbarclay@gmail.com>
2018-01-25 21:36:15 -0800

    Add passed tests for Board.js

--- src/solvers.js
+++ src/solvers.js
@@ -16,11 +16,40 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({n: n}); //the board, whatever it is;
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  // var rowArray = [0,0,0,0];
+  // get size of board
+  var size = n;
+  debugger;
+
+  // initialize for first row
+  var row = 0;
+  // initialize for first col
+  var col = 0;
+
+
+  for (var row = 0; row < size; row++) {
+    var recurseRow = function(row, col) {
+      if (col = n) {
+        return;
+      }
+    
+      // toggle on row,col
+      board.togglePiece(row, col); // on
+      // check for conflict
+      if (board.hasAnyRooksConflicts(row, col)) {
+        // toggle off row,col
+        board.togglePiece(row, col); // off
+        col++;
+        recurseRow(row, col);
+      }
+    };
+  }
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(board));
+  return board;
 };
+
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
@@ -34,7 +63,7 @@ window.countNRooksSolutions = function(n) {
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(board));
   return solution;
 };
 
